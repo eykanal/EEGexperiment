c5cf532a55c78ff7f8398257fcd10d1b5176e62a
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2010-05-24 11:31:19 +0000

    add data collection routine
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@23 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- UDPtesting/test_udp_syntax.m
+++ UDPtesting/test_udp_syntax.m
@@ -1,12 +1,29 @@
+%####################
+%## INITIALIZATION ##
+%####################
+
 % set up UDP messaging socket
 localIP = '10.1.1.3';
 remoteIP = '10.1.1.2';
 port = 6665;
 
-%socketTag = matlabUDP('open',localIP,remoteIP,port);
 u = udp(remoteIP, port);
 fopen(u);
 
+% set up response collection 
+parport = digitalio('parallel','LPT1');
+hwlines = addline(parport,0:7,0,'out');
+hwlines = addline(parport,0:4,1,'in');
+hwlines = addline(parport,0:3,2,'in');
+% set line value to 0 at start
+putvalue(parport.Line(1:8), 0);
+
+
+%#####################
+%## MAIN EXPERIMENT ##
+%#####################
+
+% set vars
 diameter =  0.3;
 color =     [255 255 0];
 
@@ -14,14 +31,15 @@ color =     [255 255 0];
 arguments = [diameter color];
 
 % demo code for showing four sets of dots and targets
-fprintf(u,'myTarg1    = rAdd(''dXtarget'',1,''visible'',true,''diameter'',2,''penWidth'',24,''color'',[0 0 0]);');
 fprintf(u,'myTarg2    = rAdd(''dXtarget'',1,''visible'',true,''diameter'',%d,''penWidth'',24,''color'',[%d %d %d]);', arguments);
 fprintf(u,'dotsIdx    = rAdd(''dXdots'', 1, ''color'', [255 255 255], ''direction'',0,''coherence'',10, ''diameter'',10,''size'',floor(rGet(''dXscreen'', 1, ''pixelsPerDegree'')*0.15),''loops'',3,''density'',20,''speed'',7,''lifetimeMode'',''random'', ''visible'',false);');
 fprintf(u,'rSet(''dXdots'',dotsIdx,''direction'',0,''visible'',true);');
 fprintf(u,'rSet(''dXtarget'',myTarg1,''visible'',true);');
 fprintf(u,'rSet(''dXtarget'',myTarg2,''visible'',true);');
 fprintf(u,'draw_flag=1;');
-WaitSecs(2);
+
+% wait for response
+
 fprintf(u,'draw_flag=0;');
 fprintf(u,'rGraphicsBlank;');
 fprintf(u,'continue_flag=false;');
