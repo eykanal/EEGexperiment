b796cb5745a91d5a833c3909ba25574162891fba
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2010-06-09 09:28:35 +0000

    rewrite using `execute_code`
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@40 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- welcome_quest.m
+++ welcome_quest.m
@@ -1,7 +1,12 @@
-function welcome_quest();
-
+function welcome_quest()
 
 try
+    % pull in vars from global space
+    Behavioral  = evalin('caller','Behavioral');
+    u           = evalin('caller','u');
+    parport     = evalin('caller','parport');
+    
+    % first instruction screen
     lnsp = -1;  
     str = {};
     x_pos = {};
@@ -18,16 +23,17 @@ try
     str{11} = 'your response';
     str{12} = '(Press the ''Space'' key to continue.)';
 
-    [wn_, sr_, ppd_, fr_] = rGraphicsGetScreenAttributes;
+    if Behavioral
+        [wn_, sr_, ppd_, fr_] = rGraphicsGetScreenAttributes;
+    end
+    
     xres = sr_(3);
     yres = sr_(4);
 
-    fontsize = 22;
     fontsize = 30;    
 
     for i = 1:length(str)
         textwidth = fontsize*length(str{i})/ppd_;
-%         x_pos{i} = -textwidth/pi;
         x_pos{i} = -0.3*textwidth;
     end
 
@@ -37,27 +43,33 @@ try
     end
     y = y*lnsp;
 
-    % show a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext', length(str), ...
+    args = {@rAdd, ...
+        'dXtext', length(str), ...
         'visible', true, ...
         'x', x_pos, 'y', num2cell(y), ...
         'size', fontsize, ...
         'font', 'Courier', ...
         'bold', true, ...
         'color', [1 1 1]*255, ...
-        'string', str );
-
-    spacekey = 44; % Only accept a space as the command to quit:
-    [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
-    while KbCheckMulti
-        WaitSecs(0.002);
+        'string', str };
+    execute_code(Behavioral,args,u,'indices');
+    
+    if Behavioral
+        spacekey = 44;
+        [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
+        while KbCheckMulti
+            WaitSecs(0.002);
+        end
+    else
+        get_pads_response( parport );
     end
+    
+    args = { @rSet,'dXtext',indices,'visible',false};
+    execute_code( Behavioral, args, u );
+    args = { @rGraphicsDraw };
+    execute_code( Behavioral, args, u );
 
-    rSet('dXtext',indices,'visible',false);
-    rGraphicsDraw;
-
-
+    % second instruction screen
     str = {};
     x_pos = {};
     str{1} = 'When you score a point, you''ll hear a sound, and';
@@ -66,34 +78,35 @@ try
 
     for i = 1:length(str)
         textwidth = fontsize*length(str{i})/ppd_;
-%         x_pos{i} = -textwidth/pi;
         x_pos{i} = -0.3*textwidth;
     end
 
-    % shoe a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext', length(str), 'visible', true, ...
+    args = {@rAdd, ...
+        'dXtext', length(str), 'visible', true, ...
         'x', x_pos, 'y', { -1*lnsp 0*lnsp 5*lnsp }, ...
         'size', fontsize, ...
         'font', 'Courier',  ...
         'bold', true, ...
         'color', [1 1 1]*255, ...
-        'string', str );
-
-    %     rGraphicsDraw(inf)
-    spacekey = 44; % Only accept a space as the command to quit:
-    [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
-    while KbCheckMulti
-        WaitSecs(0.002);
+        'string', str };
+    execute_code(Behavioral,args,u,'indices');
+    
+    if Behavioral
+        spacekey = 44;
+        [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
+        while KbCheckMulti
+            WaitSecs(0.002);
+        end
+    else
+        get_pads_response( parport );
     end
-    %     KbWait;
+    
+    args = { @rSet,'dXtext',indices,'visible',false};
+    execute_code( Behavioral, args, u );
+    args = { @rGraphicsDraw };
+    execute_code( Behavioral, args, u );
 
-    %     rRemove('dXtext',indices);
-    rSet('dXtext',indices,'visible',false);
-    rGraphicsDraw;
-    %     rClear;
-
-
+    % third instruction screen
     str = {};
     x_pos = {};
     str{1} = 'Also, please try to keep your';
@@ -103,36 +116,36 @@ try
 
     for i = 1:length(str)
         textwidth = fontsize*length(str{i})/ppd_;
-    %         x_pos{i} = -textwidth/pi;
         x_pos{i} = -0.3*textwidth;
 
     end
 
-    % shoe a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext', length(str), 'visible', true, ...
+    args = {@rAdd, ...
+        'dXtext', length(str), 'visible', true, ...
         'x', x_pos, 'y', {  -1*lnsp 0*lnsp 1*lnsp 5*lnsp }, ...
         'size', fontsize, ...
         'font', 'Courier',  ...
         'bold', true, ...
         'color', [1 1 1]*255, ...
-        'string', str );
+        'string', str };
+    execute_code(Behavioral,args,u,'indices');
 
-    %     rGraphicsDraw(inf)
-    spacekey = 44; % Only accept a space as the command to quit:
-    [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
-    while KbCheckMulti
-        WaitSecs(0.002);
+    if Behavioral
+        spacekey = 44;
+        [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
+        while KbCheckMulti
+            WaitSecs(0.002);
+        end
+    else
+        get_pads_response( parport );
     end
-    %     KbWait;
+    
+    args = { @rSet,'dXtext',indices,'visible',false};
+    execute_code( Behavioral, args, u );
+    args = { @rGraphicsDraw };
+    execute_code( Behavioral, args, u );
 
-    %     rRemove('dXtext',indices);
-    rSet('dXtext',indices,'visible',false);
-    rGraphicsDraw;
-    %     rClear;
-
-
-
+    % fourth instruction screen
     str = {};
     x_pos = {};
     str{1} = 'Try not to press';
@@ -142,37 +155,37 @@ try
 
     for i = 1:length(str)
         textwidth = fontsize*length(str{i})/ppd_;
-    %         x_pos{i} = -textwidth/pi;
         x_pos{i} = -0.3*textwidth;
 
     end
 
-    % shoe a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext', length(str), 'visible', true, ...
+    args = { @rAdd, ...
+        'dXtext', length(str), 'visible', true, ...
         'x', x_pos, 'y', {  -1*lnsp 0*lnsp 1*lnsp 5*lnsp }, ...
         'size', fontsize, ...
         'font', 'Courier',  ...
         'bold', true, ...
         'color', [1 1 1]*255, ...
-        'string', str );
+        'string', str };
+    execute_code(Behavioral,args,u,'indices');
 
-    %     rGraphicsDraw(inf)
-    spacekey = 44; % Only accept a space as the command to quit:
-    [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
-    while KbCheckMulti
-        WaitSecs(0.002);
+    if Behavioral
+        spacekey = 44;
+        [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
+        while KbCheckMulti
+            WaitSecs(0.002);
+        end
+    else
+        get_pads_response( parport );
     end
-    %     KbWait;
 
-    %     rRemove('dXtext',indices);
-    rSet('dXtext',indices,'visible',false);
-    rGraphicsDraw;
-    %     rClear;
-    %     rGraphicsBlank;
+    args = { @rSet,'dXtext',indices,'visible',false};
+    execute_code( Behavioral, args, u );
+    args = { @rGraphicsDraw };
+    execute_code( Behavioral, args, u );
 
 catch
-    e = lasterror
+    e = lasterror;
 end
 
 
