c2191ce5f80041a37294b4a9fb266da6e03b1024
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2010-06-17 13:29:04 +0000

    add 1208 testing script
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@54 1025f1b0-3930-4f01-9bbb-a99de52d555f

---
+++ 1028testing/test_1208.m
@@ -0,0 +1,65 @@
+function test_1208()
+%
+% This function tests 1208 functionality by:
+%   1) locating the device
+%   2) blinking the device LED
+%   3) receiving digital data through port B
+%   4) sending a digital response out port A
+% the device.
+%
+%   EK - 6/17/10
+%
+
+% 1) locate the device
+disp('\n1) Locate device');
+devices = PsychHID('Devices');
+daq = DaqDeviceIndex;
+if ~isempty(daq)
+    disp('Found device!');
+else
+    disp('No device found!');
+    disp(devices);
+    return;
+end
+
+% 2) blinking device LED
+disp('\n2) Blink LED');
+err = DaqBlinkLED(daq);
+if ~err
+    disp('Error!');
+    disp(err);
+    return;
+end
+
+resp = input('Did it blink? (y/n)');
+if resp ~= 'y'
+    disp('I thought it did! Problem!');
+    return;
+else
+    disp('Good!');
+end
+
+% 3) receive data
+disp('\n3) Receive data');
+errA = DaqDConfigPort(daq, 0, 0);   % Port A will send data out
+errB = DaqDConfigPort(daq, 1, 1);   % Port B will receive data
+
+begTime = GetSecs;
+data = DaqDIn(daq);
+disp('Please press a pad button...');
+resp = data;
+while resp == data;
+    resp = DaqDIn(daq);
+    if (GetSecs - begTime) > 30
+        disp('Not receiving data! Bummer!');
+        return
+    end
+end
+disp('Received data!');
+disp(resp);
+
+% 4) send data out
+disp('\n4)  Send data');
+err1 = DaqDOut(daq, 0, 10);
+WaitSecs(0.5);
+err2 = DaqDIn(daq, 0, 5);
