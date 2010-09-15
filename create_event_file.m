2c06fa5dcb8fd869e4915479ba7be5be0ad5529b
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2010-09-15 16:47:13 +0000

    small fixes, typo correction, add response direction averaging file creation
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@89 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- create_event_file.m
+++ create_event_file.m
@@ -33,21 +33,21 @@ times.onset    = data.times(data.values == 1);
 times.offset   = data.times(data.values == 2);
 
 % see VoodooPad for description of variables
-load(subj_data, 'coherence_array', 'cohVec', 'cueVec', 'arrow_direction',...
-    'ER', 'RT', 'Left_RT', 'Right_RT');
+load(subj_data, 'coherence_array', 'cohVec', 'cueVec', 'ST', 'ER', 'RT',...
+    'Left_RT', 'Right_RT', 'RDir');
 
 %
 % Sanity checks
 %
 
 % Number of trigger onsets should equal number of offsets.
-if length(times.onset) ~= length(times.offset)
+if length(data.times.onset) ~= length(data.times.offset)
     error('Trigger onset and offset vectors are nonequal length!');
 end
 
 % The number of triggers in the MEG file should exactly match the length of
 % the number of trials (as measured by the coherence vector).
-if length([cohVec Right_RT Left_RT]) ~= length(times.onset)
+if length([cohVec Right_RT Left_RT]) ~= length(data.times.onset)
     error('PsychToolbox and STI101 trigger onsets report unequal number of trials!');
 end
 
@@ -57,29 +57,39 @@ end
 
 % Average based on coherence. Only include (1) correct, (2) non-arrow
 % trials.
-coherence_entries.times.onset  = times.onset (and(cueVec == 'd', ER == 0));
-coherence_entries.times.offset = times.offset(and(cueVec == 'd', ER == 0));
+coherence_entries.times.onset  = data.times.onset (cueVec == 'd' & ER == 0);
+coherence_entries.times.offset = data.times.offset(cueVec == 'd' & ER == 0);
 
 coherence_entries.coherence = ones(size(coherence_entries.times.onset));  % set all coherence entries to "high"... gotta start somewhere!
-coherence_entries.coherence(cohVec(and(cueVec == 'd', ER == 0)) == min(coherence_array)) = 2;  % change the low ones to "low"
+coherence_entries.coherence(cohVec(cueVec == 'd' & ER == 0) == min(coherence_array)) = 2;  % change the low ones to "low"
 
 coherence_entries.labels = repmat({'high coherence'}, size(coherence_entries.coherence));
 coherence_entries.labels(coherence_entries.coherence == 2) = {'low coherence'};
 
+% Average based on response direction
+respdir_entries.times.onset  = data.times.onset (cueVec == 'd');
+respdir_entries.times.offset = data.times.offset(cueVec == 'd');
+
+respdir_entries.direction = ones(size(respdir_entries.times.onset));
+respdir_entries.direction(RDir == 'R') = 2;
+
+respdir_entries.labels = repmat({'left'}, size(respdir_entries.direction));
+respdir_entries.labels(respdir_entries.direction == 2) = {'right'};
+
 % Average based on arrows. Only include (1) correct, (2) arrow trials.
-arrows_entries.times.onset  = times.onset (and(cueVec == 'a', ER == 0));
-arrows_entries.times.offset = times.offset(and(cueVec == 'a', ER == 0));
+arrows_entries.times.onset  = data.times.onset (cueVec == 'a' & ER == 0);
+arrows_entries.times.offset = data.times.offset(cueVec == 'a' & ER == 0);
 
 arrows_entries.direction = ones(size(arrows_entries.times.onset));
-%arrows_entries.direction(arrow_direction == 180) = 2;
-
+arrows_entries.direction(ST == 'R') = 2;
+ 
 arrows_entries.labels = repmat({'right'}, size(arrows_entries.direction));
 arrows_entries.labels(arrows_entries.direction == 2) = {'left'};
 
 % Average the signal detection routine.
 sigdet_RT = [Left_RT Right_RT];
-sigdet_entries.times.onset  = times.onset (length(cohVec)+1 : length(cohVec)+length(sigdet_RT));
-sigdet_entries.times.offset = times.offset(length(cohVec)+1 : length(cohVec)+length(sigdet_RT));
+sigdet_entries.times.onset  = data.times.onset (length(cohVec)+1 : length(cohVec)+length(sigdet_RT));
+sigdet_entries.times.offset = data.times.offset(length(cohVec)+1 : length(cohVec)+length(sigdet_RT));
 
 sigdet_entries.direction = ones(size(sigdet_RT));
 sigdet_entries.direction(1:length(Left_RT)) = 2;
@@ -92,20 +102,25 @@ sigdet_entries.labels(sigdet_entries.direction == 2) = {'right'};
 %
 
 % open/create coherence event file for writing
-[path,filename] = fileparts(which(hdr.info.filename));
+%[path,filename] = fileparts(which(hdr.info.filename));
+path = '/Users/eliezerk/Documents/Research-postdoc/meg_data/Dots/4_072910/';
+filename = 'dj_072910_1';
 
 fid_coh_stim    = fopen(fullfile(path, sprintf('%s_coh_stim.eve',filename)),'w+');
 fid_coh_resp    = fopen(fullfile(path, sprintf('%s_coh_resp.eve',filename)),'w+');
+fid_respdir_stim   = fopen(fullfile(path, sprintf('%s_respdir_stim.eve',filename)),'w+');
+fid_respdir_resp   = fopen(fullfile(path, sprintf('%s_respdir_resp.eve',filename)),'w+');
 fid_arrows_stim = fopen(fullfile(path, sprintf('%s_arrows_stim.eve',filename)),'w+');
-fid_arrows_resp = fopen(fullfile(path, sprintf('%s_aarows_resp.eve',filename)),'w+');
+fid_arrows_resp = fopen(fullfile(path, sprintf('%s_arrows_resp.eve',filename)),'w+');
 fid_sigdet_stim = fopen(fullfile(path, sprintf('%s_sigdet_stim.eve',filename)),'w+');
 fid_sigdet_resp = fopen(fullfile(path, sprintf('%s_sigdet_resp.eve',filename)),'w+');
 
 % determine error rate
+ER_condition = sum(cueVec=='a' & ER == 0)/sum(cueVec=='a');
+fprintf('Error rate for arrows = %0.4f\n', ER_condition);
 for n = 1:length(coherence_array)
-    ER_condition = ER(cohVec == coherence_array(n));
-    
-    fprintf('Error rate for coherence %d = %0.3f\n', coherence_array(n), mean(ER_condition));
+    ER_condition = sum(cueVec=='d' & ER==0 & cohVec==coherence_array(n))/sum(cueVec=='d' & cohVec==coherence_array(n));
+    fprintf('Error rate for coherence %d = %0.4f\n', coherence_array(n), ER_condition);
 end
 
 % save event file in following format:
@@ -115,6 +130,8 @@ end
 % write the coherence event file
 fprintf(fid_coh_stim,'0\t0\t0\t0\n');
 fprintf(fid_coh_resp,'0\t0\t0\t0\n');
+fprintf(fid_respdir_stim,'0\t0\t0\t0\n');
+fprintf(fid_respdir_resp,'0\t0\t0\t0\n');
 fprintf(fid_arrows_stim,'0\t0\t0\t0\n');
 fprintf(fid_arrows_resp,'0\t0\t0\t0\n');
 fprintf(fid_sigdet_stim,'0\t0\t0\t0\n');
@@ -124,6 +141,12 @@ fprintf(fid_sigdet_resp,'0\t0\t0\t0\n');
 for n = 1:length(coherence_entries.times.onset)
 	fprintf(fid_coh_stim, '%8i\t%5.3f\t%i\t%i\t%s\n', coherence_entries.times.onset(n),  coherence_entries.times.onset(n)/1000,  0, coherence_entries.coherence(n), mat2str(cell2mat(coherence_entries.labels(n))));
     fprintf(fid_coh_resp, '%8i\t%5.3f\t%i\t%i\t%s\n', coherence_entries.times.offset(n), coherence_entries.times.offset(n)/1000, 0, coherence_entries.coherence(n), mat2str(cell2mat(coherence_entries.labels(n))));
+end
+
+% write response direction file
+for n = 1:length(respdir_entries.times.onset)
+    fprintf(fid_respdir_stim, '%8i\t%5.3f\t%i\t%i\t%s\n', respdir_entries.times.onset(n),  respdir_entries.times.onset(n)/1000,  0, respdir_entries.direction(n), mat2str(cell2mat(respdir_entries.labels(n))));
+    fprintf(fid_respdir_resp, '%8i\t%5.3f\t%i\t%i\t%s\n', respdir_entries.times.offset(n), respdir_entries.times.offset(n)/1000, 0, respdir_entries.direction(n), mat2str(cell2mat(respdir_entries.labels(n))));
 end
 
 % write arrows file
@@ -140,6 +163,8 @@ end
 
 fclose(fid_coh_stim);
 fclose(fid_coh_resp);
+fclose(fid_respdir_stim);
+fclose(fid_respdir_resp);
 fclose(fid_arrows_stim);
 fclose(fid_arrows_resp);
 fclose(fid_sigdet_stim);
