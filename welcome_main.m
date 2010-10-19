300f04d2287fb9a2d882125ec5d1e9905c8108ae
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2010-10-19 16:00:17 +0000

    completely rewrote to use loop, not using this file in experiment_eeg anymore anyway
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@107 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- welcome_main.m
+++ welcome_main.m
@@ -1,157 +1,77 @@
 
 function welcome_main()
 
-
 % show some text with the dXtext class
 % also a good way to find text that looks good on a particular screen
 
-try
-    lnsp = -1;
+strings{1}{1} = 'Now your task is to gain as many points';
+strings{1}{2} = 'as possible. The experiment has a fixed';
+strings{1}{3} = 'duration, so the faster you go, the more';
+strings{1}{4} = 'money you can make. It''s OK to be greedy!';
+strings{1}{5} = '';
+strings{1}{6} = '(Press the ''Space'' key to continue.)  ';    
 
-    str{1}= 'Now your task is to gain as many        ';
-    str{2}= 'points as possible.                     ';
-    str{3}= 'The experiment has a fixed duration. So ';
-    str{4}= 'the faster you go, the more money you   ';
-    str{5}= 'can make. It''s OK to be greedy!:)      ';
-    str{6}= '(Press the ''Space'' key to continue.)  ';    
+strings{2}{1} = 'Your task is to indicate the direction of the';
+strings{2}{2} = 'motion or arrows (depending on the block) with';
+strings{2}{3} = 'Z for left ';
+strings{2}{4} = 'M for right';
+strings{2}{5} = '';
+strings{2}{6} = '(Press the ''Space'' key to continue.)';
+
+strings{3}{1} = 'When you score a point, you''ll hear a';
+strings{3}{2} = 'sound, and the circle or triangle will';
+strings{3}{3} = 'turn green. For an error it will turn';
+strings{3}{4} = 'red. You''ll see your current score every';
+strings{3}{5} = '5 trials.';
+strings{3}{6} = '';
+strings{3}{7} = '(Press the ''Space'' key to continue.)';
+
+strings{4}{1} = 'Do whatever necessary to make as much money';
+strings{4}{2} = 'as possible.';
+strings{4}{3} = 'Z = left ';
+strings{4}{4} = 'M = right';
+strings{4}{5} = '';
+strings{4}{6} = '(Press the ''Space'' key to continue.)';
+
+[wn_, sr_, ppd_, fr_] = rGraphicsGetScreenAttributes;
+font_degrees = 0.7;
+fontsize = floor(font_degrees * ppd_);
+
+% display each instruction set
+for n = 1:length(strings)
+    str   = [];
+    x_pos = [];
+    y_pos = [];
+
+    str = strings{n};
     
-    [wn_, sr_, ppd_, fr_] = rGraphicsGetScreenAttributes;
-
-    fontsize = 22;
-    fontsize = 30;
-
     for i = 1:length(str)
-        textwidth = fontsize*length(str{i})/ppd_;
-        x_pos{i} = -0.3*textwidth;
+        textwidth = fontsize * length(str{i})/ppd_;
+        x_pos{i}  = -0.3 * textwidth;
     end
-
-    % show a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext', length(str), 'visible', true, ...
-        'x', x_pos, 'y', {-3*lnsp -2*lnsp -1*lnsp 0*lnsp 1*lnsp 4*lnsp }, ...
-        'size', fontsize, ...
-        'font', 'Courier',  ...
-        'bold', true, ...
-        'color', [1 1 1]*255, ...
-        'string', str);
-
-    spacekey = 44; % Only accept a space as the command to quit:
-    [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
-    while KbCheckMulti
-        WaitSecs(0.002);
-    end
-
     
-    rSet('dXtext',indices,'visible',false);
-    rGraphicsDraw;
- 
-    str = {};
-    x_pos = {};
-    str{1} = 'Your task is to indicate the direction of the ';
-    str{2} = 'motion or arrows (depending on the block) with';
-    str{3} = 'Z for left';
-    str{4} = 'M for right';
-    str{9} = '(Press the ''Space'' key to continue.)';
-
-    for i = 1:length(str)
-        textwidth = fontsize*length(str{i})/ppd_;
-        x_pos{i} = -0.3*textwidth;
-    end
-
-   y = -ceil(length(str)/2):floor(length(str)/2);
-    if length(y)>length(str)
-      y = y(1:length(str));
-    end
-    y = y*lnsp;    
+    % Equally distribute lines in middle of screen
+    y_pos = font_degrees * (length(str):-1:1) - (floor(length(str)/2));
+    y_pos = num2cell(y_pos);
     
-    % shoe a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext',length(str), 'visible', true, ...
-        'x', x_pos, 'y', num2cell(y), ...
-        'size', fontsize, ...
-        'font', 'Courier',  ...
-        'bold', true, ...
-        'color', [1 1 1]*255, ...
-        'string', str );
+    indices = rAdd( ...
+        'dXtext',   length(str), ...
+        'visible',  true, ...
+        'x',        x_pos, ...
+        'y',        y_pos, ...
+        'size',     fontsize, ...
+        'font',     'Courier',  ...
+        'bold',     true, ...
+        'color',    [1 1 1]*255, ...
+        'string',   str);
 
-    spacekey = 44; % Only accept a space as the command to quit:
+    spacekey = 44;  % Only accept a space as the command to quit:
     [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
+
     while KbCheckMulti
         WaitSecs(0.002);
     end
 
     rSet('dXtext',indices,'visible',false);
     rGraphicsDraw;
-    str = {};
-    x_pos = {};
-    str{1} = 'When you score a point, you''ll hear a sound, and';
-    str{2} = 'the circle or triangle will turn green. For an   ';
-    str{3} = 'error it will turn red.                          ';
-    str{4} = 'You''ll see your current score every 5 trials.   ';
-    str{5} = '(Press the ''Space'' key to continue.)';
-
-    for i = 1:length(str)
-        textwidth = fontsize*length(str{i})/ppd_;
-        x_pos{i} = -0.3*textwidth;
-    end
-
-    % show a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext', 5, 'visible', true, ...
-        'x', x_pos, 'y', { -1*lnsp 0*lnsp 1*lnsp 2*lnsp 5*lnsp }, ...
-        'size', fontsize, ...
-        'font', 'Courier',  ...
-        'bold', true, ...
-        'color', [1 1 1]*255, ...
-        'string', str );
-
-%     rGraphicsDraw(inf)
-    spacekey = 44; % Only accept a space as the command to quit:
-    [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
-    while KbCheckMulti
-        WaitSecs(0.002);
-    end
-    %     KbWait;
-
-    rRemove('dXtext',indices);
-%     rSet('dXtext',indices,'visible',false);
-    rGraphicsDraw;
-    %     rClear;
-    
-
-    str = {};
-    x_pos = {};
-    str{1} = 'Do whatever necessary to make as much money as';
-    str{2} = 'possible.';
-    str{3} = 'Z = left';
-    str{4} = 'M = right';
-    str{5} = '(Press the ''Space'' key to continue.)';
-
-
-    for i = 1:length(str)
-        textwidth = fontsize*length(str{i})/ppd_;
-        x_pos{i} = -0.3*textwidth;
-    end
-
-    % shoe a normal text, and
-    %   try to make another that looks good on mono++
-    indices = rAdd('dXtext', length(str), 'visible', true, ...
-        'x', x_pos, 'y', {  -1*lnsp 0*lnsp 1*lnsp 2*lnsp 5*lnsp }, ...
-        'size', fontsize, ...
-        'font', 'Courier',  ...
-        'bold', true, ...
-        'color', [1 1 1]*255, ...
-        'string', str );
-
-    spacekey = 44; % Only accept a space as the command to quit:
-    [ keySecs, myKeyCode ] = rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
-    while KbCheckMulti
-        WaitSecs(0.002);
-    end
-    rSet('dXtext',indices,'visible',false);
-    rGraphicsDraw;
-catch
-    e = lasterror 
 end
-
-
