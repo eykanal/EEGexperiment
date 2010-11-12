e6bae305c6bda068eea76cec99527dafe398432a
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2010-11-12 14:57:33 +0000

    - add daq code - add recordToMEG parameter - reverse saved y-axis data
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@129 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- photodiodeTest/photodiodeTest.m
+++ photodiodeTest/photodiodeTest.m
@@ -1,3 +1,22 @@
+% Initialize the USB device:
+global daq;
+
+close all;
+
+devices = PsychHID('Devices');
+daq = DaqDeviceIndex;
+
+if daq > 0
+    % fix for macbook bug
+    if length(daq) > 1
+        daq = 4;
+    end
+    errA = DaqDConfigPort(daq, 0, 0);   % Port A will send data out
+    errB = DaqDConfigPort(daq, 1, 1);   % Port B will receive data
+else
+    error('Please plug in the Daq device!');
+end
+
 Screen('Preference','SkipSyncTests',0);
 rInit({'screenMode','local','showWarnings',true});
 
@@ -6,17 +25,21 @@ dotsIdx         = rAdd( ...
     'color',    [255 255 255], ...
     'direction',0, ...
     'coherence',25, ...
-    'diameter', 1, ...
-    'size',     8, ...
+    'diameter', 10, ...
+    'size',     5, ...
     'loops',    1, ...
     'density',  20, ...
     'speed',    7, ...
     'lifetimeMode', 'random', ...
     'visible',  true, ...
-    'debugSavePts', true ...
+    'debugSavePts', true, ...
+    'recordToMEG', true ...
 );
 
-a = rGraphicsDraw(1);
+a = rGraphicsDraw(5000);
+
+% stored upside-down; reverse the y axis
+a(2,:,:) = 1-a(2,:,:);
 
 rDone;
 
@@ -31,4 +54,4 @@ scatter(x,y);
 hold on;
 scatter(x,-y+1);
 
-scatter(a(1,1,:),a(2,1,:))
+% scatter(a(1,:,time),a(2,:,time)) plots all points for a given frame
