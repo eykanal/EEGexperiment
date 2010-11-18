72c1776f45a54f2c3282fd641b93495398759c93
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2010-11-18 12:17:27 +0000

    - add routine for localizer dot - reset rand stream so always does same dot pattern - save dot onset time as well as dot position - save results to file
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@133 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- photodiodeTest/photodiodeTest.m
+++ photodiodeTest/photodiodeTest.m
@@ -1,10 +1,18 @@
-function photodiodeTest( filename )
+function photodiodeTest( varargin )
 
 % Initialize the USB device:
 global daq;
 
+if length(varargin) > 0
+    filename = varargin(1);
+else
+    filename = '';
+end
+
+% close all open images
 close all;
 
+% set up box for MEG
 devices = PsychHID('Devices');
 daq = DaqDeviceIndex;
 
@@ -16,21 +24,53 @@ if daq > 0
     errA = DaqDConfigPort(daq, 0, 0);   % Port A will send data out
     errB = DaqDConfigPort(daq, 1, 1);   % Port B will receive data
 else
-    error('Please plug in the Daq device!');
+    warning('photodiode:noDaqDevice','No Daq device detected. If scanning, ensure that all settings are correct.');
 end
 
 Screen('Preference','SkipSyncTests',0);
 rInit({'screenMode','local','showWarnings',true});
 
+% Put up the localizer dot
+direction   = 0;
+coherence   = 25;
+diameter    = 1;
+size        = 25;
+loops       = 3;
+density     = 20000;
+speed       = 7;
+
+localizer = rAdd( ...
+    'dXdots',   1, ...
+    'color',    [255 255 255], ...
+    'direction',direction, ...
+    'coherence',coherence, ...
+    'diameter', diameter, ...
+    'size',     size, ...
+    'loops',    loops, ...
+    'density',  density, ...
+    'speed',    speed, ...
+    'lifetimeMode', 'random', ...
+    'visible',  true, ...
+    'recordToMEG', true ...
+);
+
+spacekey = 44;
+rGraphicsDrawSelectiveBreakMulti(inf,[],spacekey);
+rGraphicsBlank;
+rClear;
+
+WaitSecs(1);
+
+% Set up actual dots
 direction   = 0;
 coherence   = 25;
 diameter    = 10;
 size        = 5;
-loops       = 1;
+loops       = 3;
 density     = 20;
 speed       = 7;
 
-dotsIdx         = rAdd( ...
+testDisplay = rAdd( ...
     'dXdots',   1, ...
     'color',    [255 255 255], ...
     'direction',direction, ...
@@ -46,13 +86,41 @@ dotsIdx         = rAdd( ...
     'recordToMEG', true ...
 );
 
-dot_history = rGraphicsDraw(5000);
+% Reset the rand state so we always get the same dots pattern
+c = RandStream.create('mt19937ar');  % twister, seed 0
+RandStream.setDefaultStream(c);
+c.reset;
+
+% Draw stuff
+[dot_history dot_timeHistory] = rGraphicsDraw(5000);
+
+rGraphicsBlank;
 
 % stored upside-down; reverse the y axis
 dot_history (2,:,:) = 1 - dot_history (2,:,:);
+% normalize time history s.t. t_0 = 0;
+dot_timeHistory = dot_timeHistory - min(dot_timeHistory);
 
-fname = sprintf('photodiodeTestData_%s.mat', filename);
-save(fname, 'direction', 'coherence', 'diameter', 'size', 'loops', 'density', 'speed', 'dot_history');
+finishedSaving = false;
+while finishedSaving == false
+    fname = sprintf( 'photodiodeTestData_%s.mat', filename );
+    if ~exist(fname, 'file')
+        save(fname, 'direction', 'coherence', 'diameter', 'size', 'loops', 'density', 'speed', 'dot_history', 'dot_timeHistory');
+        finishedSaving = true;
+    else
+        if ~strcmp( input( sprintf( 'File exists! (%s) Overwrite? (Y/n): ', fname ), 's' ), 'n' )
+            delete(fname);
+            save(fname, 'direction', 'coherence', 'diameter', 'size', 'loops', 'density', 'speed', 'dot_history', 'dot_timeHistory');
+            finishedSaving = true;
+        else
+            if ~strcmp( input( 'Save with new name? (Y/n): ', 's' ), 'n' )
+                filename = input ( 'New file name: ', 's' );
+            else
+                finishedSaving = true;
+            end
+        end
+    end
+end
 
 rDone;
 
@@ -66,5 +134,9 @@ figure(1);
 scatter(x,y);
 hold on;
 scatter(x,-y+1);
+scatter(dot_history(1,:,1),dot_history(2,:,1));
+
+figure(2);
+plot(dot_timeHistory);
 
 end
