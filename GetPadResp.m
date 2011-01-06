b5b46b0da94201ca21ddbf01272e77208c782474
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2011-01-06 18:29:39 +0000

    delete one added yesterday, copy (from playParallel) working version to root directory
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@166 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- GetPadResp.m
+++ GetPadResp.m
@@ -1,21 +1,23 @@
 function keys = GetPadResp( deviceNumber )
 
+% define global daq value for the KbCheckMulti script
+global daq;
+daq = deviceNumber;
+
 keys = struct;
-a = GetSecs;
 
 while 1
     % Check the state of the keyboard.
-    [ keyIsDown, secs, keyCode ] = KbCheck( deviceNumber );
+    [ keyIsDown, secs, keyCode ] = KbCheckMulti( deviceNumber );
 
     % If the user is pressing a key, then display its code number and name.
     if keyIsDown == 1
         ptr = fopen( '/tmp/resp.txt', 'w' );
-        fprintf( ptr, '1,%1.4e,%i', secs, find( keyCode ) );
+        fprintf( ptr, '1,%1.3f,%i', secs, find( keyCode ) );
         fclose( ptr );
             
-        while KbCheck; end  % wait for keys to be released
+        while KbCheckMulti; end  % wait for keys to be released
     end
-end   
-
+end
 
 end
