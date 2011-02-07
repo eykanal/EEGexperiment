21505fce16140015eda184fa0a974b1119ca0271
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2011-02-07 21:02:08 +0000

    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@178 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- processing/dots_find_money.m
+++ processing/dots_find_money.m
@@ -1,9 +1,40 @@
-function dots_find_money(data_dir, meg_hrs)
-
-% # GET LIST OF EACH FILE IN DIRECTORY
+function [money] = dots_find_money(subject, mri, meg_hrs)
 %
-% # LOAD money VAR FROM EACH DATAFILE IN DIRECTORY - should not fail if
-%       money is missing
+% function dots_find_money(data_dir, meg_hrs)
 %
-% # ADD CASH - $10 flat for MRI, 15/hr for MEG
+% Function to determine how much money a subject has accumulated over the
+% course of the experiment.
+%   subject     subject ID
+%   mri         1 if MRI done, 0 otherwise
+%   meg_hrs     number of hours of MEG time subject has logged
+%
+% Eliezer Kanal, 2/7/11
 
+filepath    = ['/Users/eliezerk/Documents/MATLAB/EEGExperiment/data/subj' num2str(subject)];
+files       = dir(filepath);
+
+money = 0;
+
+% skip the first three (., .., .DS_Store)
+for file = 4:length(files)
+    % if (1) the file is a mat file and (2) contains the variable "money"
+    if strcmp( files(file).name(end-2:end), 'mat') ...
+            && ~isempty( whos( '-file', [filepath '/' files(file).name], 'money' ) )
+        t_money = load( [filepath '/' files(file).name], 'money' );
+        money = money + t_money.money;
+        
+        % add $6 for each behavioral, check if behav via presence of the
+        % ERpsych variable
+        if ~isempty( whos( '-file', [filepath '/' files(file).name], 'ERpsych' ) )
+            money = money + 600;
+        end
+    end
+end
+
+% add $10.00 for MRI
+if mri
+    money = money + 1000; 
+end
+
+% add 15/hr for MEG
+money = money + (meg_hrs * 15);
