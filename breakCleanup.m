79d2fa7d6dcdb3f6521533dd9c78652953864230
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2011-02-16 22:04:50 +0000

    conditionally destroy all leftover scheduler objects
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@191 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- breakCleanup.m
+++ breakCleanup.m
@@ -6,5 +6,18 @@ Screen CloseAll;
 % close running threads
 rsc = findResource;
 [pending queued running finished] = findJob(rsc);
-destroy(running);
+if length(pending) > 1
+    destroy(pending);
+end
+if length(queued) > 1
+    destroy(queued);
+end
+if length(running) > 1
+    destroy(running);
+end
+if length(finished) > 1
+    destroy(finished);
+end
+
+
 clear rsc pending queued running finished;
