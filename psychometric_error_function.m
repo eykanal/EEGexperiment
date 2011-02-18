f791100d5350c9f3e2fdbef1b4bf13735ac725d9
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2011-02-18 18:54:25 +0000

    whitespace cleanup
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@193 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- psychometric_error_function.m
+++ psychometric_error_function.m
@@ -1,4 +1,5 @@
 function my_error = psychometric_error_function( x, sigs, mean_behavior, cdf_type )
+% psychometric_error_function(x, coherVec, acc', cdf_type)
 
 my_error = 0;
 
@@ -6,9 +7,9 @@ my_error = 0;
 % sigs = [ -1.5000   -1.0000   -0.5000         0    0.5000    1.0000    1.5000 ];
 
 if cdf_type == 1
-   my_error = sum((mean_behavior - normcdf(x(1)*sigs+x(2))).^2);
+    my_error = sum( ( mean_behavior - normcdf( x(1) * sigs + x(2) ) ) .^2 );
 elseif cdf_type == 2
-    my_error = sum((mean_behavior -  wblcdf(sigs-sigs(1),x(1),x(2))).^2);
+    my_error = sum( ( mean_behavior - wblcdf( sigs - sigs(1), x(1), x(2) ) ) .^2 );
 elseif cdf_type == 3
-    my_error = sum((mean_behavior - gamcdf(sigs,x(1),x(2))).^2);
+    my_error = sum( ( mean_behavior - gamcdf( sigs, x(1), x(2) ) ) .^2 );
 end
