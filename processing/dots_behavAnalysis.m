20a023ef4fc3402d4f46cc4e5872d45cf8485d1e
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2011-02-22 19:50:44 +0000

    remove nargout, use disp to show results
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@199 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- processing/dots_behavAnalysis.m
+++ processing/dots_behavAnalysis.m
@@ -1,4 +1,4 @@
-function [all_num numbers] = dots_behavAnalysis(subj, ses, run)
+function dots_behavAnalysis(subj, ses, run)
 % [numbers all_num] = dots_behavAnalysis(subj, ses, run)
 %
 % subj  = subject id
@@ -44,10 +44,10 @@ for n = min(blocknum):max(blocknum)
     numbers(row+1,:) = {ses n type diff num perf rt rt_std};
 end
 
-hard_num = length( ER( cueVec == 'd' & cohVec == hard ) );
-easy_num = length( ER( cueVec == 'd' & cohVec == easy ) );
-hard_perf = 1 - mean( ER( cueVec == 'd' & cohVec == hard ) );
-easy_perf = 1 - mean( ER( cueVec == 'd' & cohVec == easy ) );
+hard_num    = length( ER( cueVec == 'd' & cohVec == hard ) );
+easy_num    = length( ER( cueVec == 'd' & cohVec == easy ) );
+hard_perf   = 1 - mean( ER( cueVec == 'd' & cohVec == hard ) );
+easy_perf   = 1 - mean( ER( cueVec == 'd' & cohVec == easy ) );
 rt_hard     = mean( RT( cueVec == 'd' & cohVec == hard ) );
 rt_hard_std = std(  RT( cueVec == 'd' & cohVec == hard ) );
 rt_easy     = mean( RT( cueVec == 'd' & cohVec == easy ) );
@@ -56,3 +56,6 @@ rt_easy_std = std(  RT( cueVec == 'd' & cohVec == easy ) );
 all_num = { 'sess', 'diff', '#',        'perf',     'RT',       'RT std'; ...
             ses,    hard,   hard_num,   hard_perf,  rt_hard,    rt_hard_std; ...
             ses,    easy,   easy_num,   easy_perf,  rt_easy,    rt_easy_std};
+        
+disp( all_num );
+disp( numbers );
