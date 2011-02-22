ef870bc408fd8a1d861d3576ee0368bbfcafdf40
elli <elli@1025f1b0-3930-4f01-9bbb-a99de52d555f>
2011-02-22 19:48:05 +0000

    - bugfix meg money calc - $5/behav only if no meg - show cumulative
    git-svn-id: svn://churro.cnbc.cmu.edu/elli/dots_task/trunk@198 1025f1b0-3930-4f01-9bbb-a99de52d555f

--- processing/dots_find_money.m
+++ processing/dots_find_money.m
@@ -1,4 +1,4 @@
-function [money] = dots_find_money(subject, mri, meg_hrs)
+function dots_find_money(subject, mri, meg_hrs)
 %
 % function dots_find_money(data_dir, meg_hrs)
 %
@@ -15,19 +15,20 @@ files       = dir(filepath);
 
 money = 0;
 
+fprintf('\n');
+
 % skip the first three (., .., .DS_Store)
 for file = 4:length(files)
     % if (1) the file is a mat file and (2) contains the variable "money"
     if strcmp( files(file).name(end-2:end), 'mat') ...
             && ~isempty( whos( '-file', [filepath '/' files(file).name], 'money' ) )
         t_money = load( [filepath '/' files(file).name], 'money' );
-        fprintf( 1, '%s - $%1.2f\n', num2str(files(file).name), t_money.money/100 );
-        
         money = money + t_money.money;
+
+        fprintf( 1, '%s\t$%1.2f\t$%1.2f\n', num2str(files(file).name), t_money.money/100, money/100);
         
-        % add $6 for each behavioral, check if behav via presence of the
-        % ERpsych variable
-        if ~isempty( whos( '-file', [filepath '/' files(file).name], 'ERpsych' ) )
+        % if didn't finish study, add $5 for each behavioral
+        if meg_hrs == 0 && ~isempty( whos( '-file', [filepath '/' files(file).name], 'ERpsych' ) )
             money = money + 500;
         end
     end
@@ -41,6 +42,6 @@ end
 
 % add 15/hr for MEG
 fprintf('from MEG: $%1.2f\n',(meg_hrs * 15));
-money = money + (meg_hrs * 15);
+money = money + (meg_hrs * 1500);
 
-fprintf( 1, '\nTotal - $%1.2f\n', money/100 );
+fprintf( 1, '\nTotal - $%1.2f\n\n', money/100 );
