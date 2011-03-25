935201864c63f7e1e02b3da38fc316088f533f1e
Eliezer Kanal <ekanal@cmu.edu>
2011-03-25 11:27:51 -0400

    update dots_ft_plot to handle multiple subtypes
    dots_ft_plot:
    - add logic to iterate through conditions

--- processing/dots_ft_avg.m
+++ processing/dots_ft_avg.m
@@ -1,32 +1,30 @@
+function dots_ft_avg(subj, sess, run, dataset)
 % Average dots fif files, both at stimulus onset and response time,
 % according to coherence and response direction, as well as averaging the
 % arrow-only trials.
 
-function dots_ft_avg(subj, sess, dataset)
 % for testing, use:
 %   subject     4
-%   session     8
-%   dataset     4_090710_4_trigFix.fif
-%   
+%   session     5
+%   dataset     4_090710_1_trigFix.fif
 
 ft_defaults;
+
+subj_data       = sprintf('subject%i_ses%i_%i', subj, sess, run);
+save_path       = sprintf('/Volumes/ShadyBackBowls/meg_data/Dots/%i/matlab-files/', subj);
+meg_full_file   = sprintf('/Volumes/ShadyBackBowls/meg_data/Dots/%i/%s', subj, dataset);
 
 %
 % Setup configuration structure
 %
 
 cfg_base            = struct;                           % ## CFG RESET! ##
-cfg_base.dataset    = dataset;
-cfg_base.hdr        = ft_read_header(cfg_base.dataset);
-
+cfg_base.dataset    = meg_full_file;
+cfg_base.hdr        = ft_read_header(meg_full_file);
 
 %
 % Define averaging parameters
 %
-
-subj_data = sprintf('subject%i_ses%i', subj, sess);
-save_path = sprintf('/Volumes/Shady\\ Back\\ Bowls/meg_data/Dots/subj%i/matlab-data/', subj);
-
 
 % variables necessary for defineDotsTrials()
 load(subj_data, 'cohVec', 'cueVec', 'ST', 'ER', 'RT', 'Left_RT', 'Right_RT', 'RDir');
@@ -39,27 +37,40 @@ if ~exist( 'Right_RT', 'var' )
 end
 
 aveTimes    = {'stim', 'resp'};
-aveParams   = {'coh', 'respdir', 'arrow'};  % currently not analyzing sigdet
+aveParams   = {{'coh', unique(cohVec)}, {'respdir','RL'}};  % currently not analyzing sigdet or arrow
 
 % average for each parameter, both at stimulus and response
 for aveTime = 1:length(aveTimes)
     for aveParam = 1:length(aveParams)
+        
+        cond = struct([struct struct]);                 % dummy variable to hold the timelock data
+                                                        % until timelockstatistics can be run
+        for aveParamValue = 1:length(aveParams{aveParam}{2})
+        
+            cfg = cfg_base;                             % ## CFG RESET! ##
+            
+            % Load data        
+            load([subj_data '-preprocessed-'  char(aveTimes(aveTime)) '-' num2str(aveParams{aveParam}{1}) '-' num2str(aveParams{aveParam}{2}(aveParamValue)) '.mat']);
 
-        % Load data        
-        load([save_path subj_data '-preprocessed-'  char(aveTimes(aveTime)) '-' char(aveParams(aveParam))]);
+            %
+            % Averaging & statistics
+            %
 
-        %
-        % Averaging & statistics
-        %
-
-        cfg.channel         = ft_channelselection( 'M*1', cfg.hdr.label );
-        cfg.keeptrials      = 'yes';
-        cfg.covariance      = 'no';
-        data_timelock       = ft_timelockanalysis(cfg, data_preprocessed);
-
-        save([subj_data '-timelock-' char(aveTimes(aveTime)) '-' char(aveParams(aveParam))], 'data_timelock');
-
-        % define all settings
+            cfg.channel     = ft_channelselection( 'M*', cfg.hdr.label );
+            cfg.keeptrials  = 'yes';
+            cfg.covariance  = 'no';
+            data_timelock   = ft_timelockanalysis(cfg, data_preprocessed);
+    
+            save([subj_data '-timelock-'  char(aveTimes(aveTime)) '-' num2str(aveParams{aveParam}{1}) '-' num2str(aveParams{aveParam}{2}(aveParamValue)) '.mat'], 'data_timelock');
+    
+            % save data for next run
+            cond(aveParamValue).data_timelock = data_timelock;
+            clear data_timelock;
+        end
+        
+        % ## TODO - Create a difference file between the conditions
+        
+        % Compare conditions using monte-carlo
         cfg = struct;                                   % ## CFG RESET! ##
         cfg.method              = 'montecarlo';         % use monte carlo to determine significance boundaries
         cfg.statistic           = 'indepsamplesT';      % indep T test determines which timepoints are actually significant
@@ -67,8 +78,8 @@ for aveTime = 1:length(aveTimes)
         cfg.alpha               = 0.025;
         cfg.tail                = 0;
         cfg.numrandomization    = 1000;
-        cfg.design              = data_timelock.trialinfo;
-        cfg.channel             = {'MEG*1'};
+        cfg.design              = [ones(size(cond(1).data_timelock.trialinfo)); 2*ones(size(cond(2).data_timelock.trialinfo))]';
+        cfg.channel             = {'M*'};
         cfg.latency             = 'all';
         cfg.clustertail         = 0;                    %  Determine significance within clusters using two-sided t-test
         cfg.ivar                = 1;
@@ -83,9 +94,10 @@ for aveTime = 1:length(aveTimes)
                                                         % generate a distribution, against which will will
                                                         % compare the original test statistic
 
-        data_timelock_stats = ft_timelockstatistics(cfg, data_timelock);
-        
-        save([save_path subj_data '-timelockstats-' char(aveTimes(aveTime)) '-' char(aveParams(aveParam))], 'data_timelock_stats');
-        clear data_preprocessed;
+        data_timelock_stats = ft_timelockstatistics(cfg, cond(1).data_timelock, cond(2).data_timelock);
+
+        save([save_path subj_data '-timelockstats-' char(aveTimes(aveTime)) '-' num2str(aveParams{aveParam}{1})], 'data_timelock_stats');
+        clear cond;
+
     end
 end
--- processing/dots_ft_plot.m
+++ processing/dots_ft_plot.m
@@ -1,4 +1,4 @@
-function dots_ft_plot(subj, sess, run)
+function dots_ft_plot(subj, sess, run, dataset)
 % function dots_ft_plot(subj, sess, run)
 % 
 % Plot output of monte carlo simulation data from ft_dots_avg
@@ -12,21 +12,18 @@ function dots_ft_plot(subj, sess, run)
 ft_defaults;
 
 %
-% Setup configuration structure
+% Setup default vars
 %
 
 cfg_base            = struct;                           % ## CFG RESET! ##
 
-
-%
-% Define averaging parameters
-%
-
 subj_data = sprintf('subject%i_ses%i_%i', subj, sess, run);
 save_path = sprintf('/Volumes/ShadyBackBowls/meg_data/Dots/%i/matlab-files/', subj);
 
+load(subj_data, 'cohVec');
+
 aveTimes    = {'stim', 'resp'};
-aveParams   = {'coh', 'respdir'};  % currently not analyzing arrows, sigdet
+aveParams   = {{'coh', unique(cohVec)}, {'respdir','RL'}};  % currently not analyzing sigdet or arrow
 
 %
 % Plot
@@ -35,61 +32,84 @@ aveParams   = {'coh', 'respdir'};  % currently not analyzing arrows, sigdet
 for aveTime = 1:length(aveTimes)
     for aveParam = 1:length(aveParams)
         
-        % after both are processed, can subtract differences and save plots
-        cond1 = load([save_path subj_data '-timelock-' char(aveTimes(aveTime)) '-' char(aveParams(aveParam)) '-1.mat'], 'data_timelock');
-        cond2 = load([save_path subj_data '-timelock-' char(aveTimes(aveTime)) '-' char(aveParams(aveParam)) '-2.mat'], 'data_timelock');
-
-        load([save_path subj_data '-timelockstats-' char(aveTimes(aveTime)) '-' char(aveParams(aveParam))], 'data_timelock_stats');
-
-        % get difference between average of conditions
-        data_timelock_diff = cond1.data_timelock;
-        data_timelock_diff.avg = cond1.data_timelock.avg - cond2.data_timelock.avg;
+        % set up to compare each subtype against each other subtype
+        comp1 = 1;
+        comp2 = 2;
+        % the logic controlling this boolean is at the end of the while
+        % loop
+        more_conditions = true;
         
-        % find significant clusters
-        pos_clusters = find( cell2mat( {data_timelock_stats.posclusters.prob} ) < data_timelock_stats.cfg.alpha );
-        neg_clusters = find( cell2mat( {data_timelock_stats.negclusters.prob} ) < data_timelock_stats.cfg.alpha );
-        % make boolean matrix of which (ch,time)-pairs are part of
-        % significant cluster (from tutorial)
-        pos = ismember( data_timelock_stats.posclusterslabelmat, pos_clusters );
-        neg = ismember( data_timelock_stats.negclusterslabelmat, neg_clusters );
+        while more_conditions
         
-        % can use following to see only first j most significant clusters
-        %pos = data_timelock_stats.posclusterslabelmat == j;
-        %neg = data_timelock_stats.negclusterslabelmat == j;
-        
-        % Plotting code comes straight from FT wiki
-        %   http://fieldtrip.fcdonders.nl/tutorial/cluster_permutation_timelock
-        
-        timestep        = 0.05;                         % timestep between time windows for each subplot (in seconds)
-        sampling_rate   = 1000;                         % Data has a temporal resolution of X Hz
-        sample_count    = length(data_timelock_stats.time);  % number of temporal samples in the statistics object
-                                
-        j = 0:timestep:0.6;                             % Temporal endpoints (in seconds) of the ERP average computed in each subplot
-        m = 1:timestep*sampling_rate:sample_count;      % temporal endpoints in MEEG samples
+            % after both are processed, can subtract differences and save plots
+            cond1 = load([save_path subj_data '-timelock-'  char(aveTimes(aveTime)) '-' num2str(aveParams{aveParam}{1}) '-' num2str(aveParams{aveParam}{2}(comp1)) '.mat'], 'data_timelock');
+            cond2 = load([save_path subj_data '-timelock-'  char(aveTimes(aveTime)) '-' num2str(aveParams{aveParam}{1}) '-' num2str(aveParams{aveParam}{2}(comp2)) '.mat'], 'data_timelock');
 
-        figure();
-        for k = 1:length(m)-1;
-            subplot(3,4,k);
-            cfg         = cfg_base;                     % ## CFG RESET! ##
-            cfg.xlim    = [j(k) j(k+1)];                % time interval of the subplot
-            cfg.zlim    = [-2.5e-13 2.5e-13];
-            % If a channel reaches this significance, then
-            % the element of pos_int with an index equal to that channel
-            % number will be set to 1 (otherwise 0).
+            load([save_path subj_data '-timelockstats-' char(aveTimes(aveTime)) '-' char(aveParams{aveParam}{1})], 'data_timelock_stats');
 
-            % Next, check which channels are significant over the
-            % entire time interval of interest.
-            pos_int     = all(pos(:, m(k):m(k+1)), 2);
-            neg_int     = all(neg(:, m(k):m(k+1)), 2);
+            % get difference between average of conditions
+            data_timelock_diff = cond1.data_timelock;
+            data_timelock_diff.avg = cond1.data_timelock.avg - cond2.data_timelock.avg;
 
-            cfg.highlight   = 'on';
-            % Get the index of each significant channel
-            cfg.highlightchannel = find(pos_int | neg_int);
-            cfg.comment     = 'xlim';   
-            cfg.commentpos  = 'title'; 
-            cfg.channel     = 'M*1';
-            cfg.layout      = 'neuromag306mag.lay';
-            ft_topoplotER(cfg, data_timelock_diff);   
+            % find significant clusters
+            pos_clusters = find( cell2mat( {data_timelock_stats.posclusters.prob} ) < data_timelock_stats.cfg.alpha );
+            neg_clusters = find( cell2mat( {data_timelock_stats.negclusters.prob} ) < data_timelock_stats.cfg.alpha );
+            % make boolean matrix of which (ch,time)-pairs are part of
+            % significant cluster (from tutorial)
+            pos = ismember( data_timelock_stats.posclusterslabelmat, pos_clusters );
+            neg = ismember( data_timelock_stats.negclusterslabelmat, neg_clusters );
+
+            % can use following to see only first j most significant clusters
+            %pos = data_timelock_stats.posclusterslabelmat == j;
+            %neg = data_timelock_stats.negclusterslabelmat == j;
+
+            % Plotting code comes straight from FT wiki
+            %   http://fieldtrip.fcdonders.nl/tutorial/cluster_permutation_timelock
+
+            timestep        = 0.05;                         % timestep between time windows for each subplot (in seconds)
+            sampling_rate   = 1000;                         % Data has a temporal resolution of X Hz
+            sample_count    = length(data_timelock_stats.time);  % number of temporal samples in the statistics object
+
+            j = 0:timestep:0.6;                             % Temporal endpoints (in seconds) of the ERP average computed in each subplot
+            m = 1:timestep*sampling_rate:sample_count;      % temporal endpoints in MEEG samples
+
+            figure();
+            for k = 1:length(m)-1;
+                subplot(3,4,k);
+                cfg         = cfg_base;                     % ## CFG RESET! ##
+                cfg.xlim    = [j(k) j(k+1)];                % time interval of the subplot
+                cfg.zlim    = [-2.5e-13 2.5e-13];
+                % If a channel reaches this significance, then
+                % the element of pos_int with an index equal to that channel
+                % number will be set to 1 (otherwise 0).
+
+                % Next, check which channels are significant over the
+                % entire time interval of interest.
+                pos_int     = all(pos(:, m(k):m(k+1)), 2);
+                neg_int     = all(neg(:, m(k):m(k+1)), 2);
+
+                cfg.highlight   = 'on';
+                % Get the index of each significant channel
+                cfg.highlightchannel = find(pos_int | neg_int);
+                cfg.comment     = 'xlim';   
+                cfg.commentpos  = 'title'; 
+                cfg.channel     = 'M*1';
+                cfg.layout      = 'neuromag306mag.lay';
+                ft_topoplotER(cfg, data_timelock_diff);   
+            end
+            
+            % doing an m x n comparison, need to set up logic to ensure
+            % that compared all possible combinations
+            if comp2 ~= length(aveParams{aveParam}{2})
+                comp2 = comp2+1;
+            else
+                if comp1 ~= length(aveParams{aveParam}{2})-1
+                    comp1 = comp1+1;
+                    comp2 = comp1+1;
+                else
+                    more_conditions = false;
+                end
+            end
         end
     end
 end
