7ad111851728ebec2eb224a6eab67e83cad530e2
Eliezer Kanal <ekanal@cmu.edu>
2011-04-18 10:08:04 -0400

    add cdRobust function

---
+++ cdRobust.m
@@ -0,0 +1,8 @@
+function success = cdRobust( directory )
+%
+% function success = cdRobust( directory )
+%
+% Move to new directory
+
+unix('cd ~/Documents/MATLAB/Psychtoolbox3/; pwd');
+cd(strtrim(b))
