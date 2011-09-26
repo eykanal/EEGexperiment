d1c05c0c46917b845291a6ccd8774f9a1063d308
Eliezer Kanal <ekanal@cmu.edu>
2011-09-26 15:57:02 -0400

    fix dots_stats_parietal
    - use grandaveraged data
    - remove nonfunctional channel fix
    - other small stuff

--- processing/dots_stats_parietal.m
+++ processing/dots_stats_parietal.m
@@ -1,28 +1,15 @@
 % load data
 subj = [];
 
-subj.h(1) = load('17-tf-resp-coh-h');
-subj.h(2) = load('18-tf-resp-coh-h');
-subj.h(3) = load('19-tf-resp-coh-h');
-
-subj.l(1) = load('17-tf-resp-coh-l');
-subj.l(2) = load('18-tf-resp-coh-l');
-subj.l(3) = load('19-tf-resp-coh-l');
-
-% account for fact that some sets have different channels selected than
-% others
-a=[];
-for n=1:2
-    for m=1:3
-        if n==1
-            a(3*(n-1)+m).chan = vertcat(subj.h(m).data_freq_varWind.label{:});
-        else
-            a(3*(n-1)+m).chan = vertcat(subj.l(m).data_freq_varWind.label{:});
-        end
-    end
+if ~exist('data_grandAvg_freq_varWind_high','var') || ~exist('data_grandAvg_freq_varWind_low','var')
+    load('resp-coh-gAvg-freqVarWind-high');
+    load('resp-coh-gAvg-freqVarWind-low');
 end
 
-path    = fileparts(which([subj.h(1) '.mat']));
+% account for fact that some sets have different channels selected than
+% others... is that necessary?
+
+path    = fileparts(which('17-tf-resp-coh-h.mat'));
 grandAvg_path = '/Volumes/ShadyBackBowls/meg_data/Dots/grandAvg/';
 
 % tf
@@ -31,6 +18,7 @@ stat_diff_freq = [8 12];
 labels = {'MEG2011', 'MEG2021', 'MEG2031', 'MEG2041'};
 sig = [];
 
+figure();
 for m=1:length(labels)
     label = labels{m};
 
@@ -45,8 +33,8 @@ for m=1:length(labels)
     mean_tf_diff = mean_tf_high - mean_tf_low;
     [sig(m).h, sig(m).p, sig(m).ci, sig(m).stats] = ttest(mean_tf_diff, 0, 0.05);
 
-    figure();
-    plot(dat1.time, squeeze(mean(mean(dat1.powspctrm(:, strcmp(dat1.label, label), dat1.freq >= 8 & dat1.freq <= 12, :),1),3)),'-xb', ...
-         dat2.time, squeeze(mean(mean(dat2.powspctrm(:, strcmp(dat2.label, label), dat2.freq >= 8 & dat2.freq <= 12, :),1),3)),'-xr');
-    title(label);
+    subplot(2,2,m);
+    plot(dat1.time, squeeze(mean(mean(dat1.powspctrm(:, strcmp(dat1.label, label), dat1.freq >= 8 & dat1.freq <= 12, :),1),3)),'-ob', ...
+         dat2.time, squeeze(mean(mean(dat2.powspctrm(:, strcmp(dat2.label, label), dat2.freq >= 8 & dat2.freq <= 12, :),1),3)),'-or');
+    title(sprintf('8-12 Hz activity over time in channel %s',label));
 end
